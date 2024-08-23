550075a94d6fbf8ce500295efd8be7d23b68889f
my name <blueshaperd3@gmail.com>
2024-08-24 01:12:33 +0500

    if statements

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let numberOfCats = prompt("How many cats?");
-let tooManyCats = Number (numberOfCats) + 1;
-if (numberOfCats === null) {
-    console.log("User canceled the prompt.");
-}
- else if (numberOfCats === "" || isNaN(numberOfCats)) {
-    console.log("Please enter a valid number of cats.");
+// Chapter 9 Prompt
+// let numberOfCats = prompt("How many cats?");
+// let tooManyCats = Number (numberOfCats) + 1;
+// if (numberOfCats === null) {
+//     console.log("User canceled the prompt.");
+// }
+//  else if (numberOfCats === "" || isNaN(numberOfCats)) {
+//     console.log("Please enter a valid number of cats.");
 
-} else {
-    console.log("Too many cats would be: " + tooManyCats);
-}
+// } else {
+//     console.log("Too many cats would be: " + tooManyCats);
+// }
+
+
+// Chapter 10 if statements
+// let x = prompt("Where does the Pope live?");
+//  if (x === "London") {
+//  alert("Correct!!!");
+//  }
+ 
+//  else {
+//     alert("Sorry, your answer is Wrong");
+//  }
