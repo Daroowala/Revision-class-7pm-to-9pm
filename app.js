71e0be310f8c7367a978d216a0b554bfbc8d6ee0
my name <blueshaperd3@gmail.com>
2024-08-24 00:36:00 +0500

    prompt is done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let numberOfCats = prompt("How many cats?");
 let tooManyCats = Number (numberOfCats) + 1;
 if (numberOfCats === null) {
-    console.log("User canceled the prompt.");}
-else if (numberOfCats === (numberOfCats)) {
+    console.log("User canceled the prompt.");
+}
+ else if (numberOfCats === "" || isNaN(numberOfCats)) {
     console.log("Please enter a valid number of cats.");
+
 } else {
     console.log("Too many cats would be: " + tooManyCats);
-} 
-
+}
